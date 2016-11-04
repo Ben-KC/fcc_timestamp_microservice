24e2a14774be270cfca9ba2e98babbf6c0827c43
Benjamin Cox <benny24c@gmail.com>
2016-11-04 08:02:16 +0000

    basic functionality

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 var express = require('express');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//month array
+var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+//home page
+app.get('/', function(req, res) {
+    
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+//handle the get request
+app.get('/:date', function(req, res) {
+    //store request
+    var input = req.params.date;
+    var date;
+//TODO handle incorrect request (return nulls) and write an index.html    
+    //check format
+    if(parseInt(input)){
+        date = new Date(parseInt(input)*1000);       
+    } else {
+        date = new Date(input);
+    }
+
+    var dateObj = {
+        unix: date.getTime(),
+        natural: months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear()
+    }
+    
+    res.send(JSON.stringify(dateObj));
 });
+
+app.listen("8080");
